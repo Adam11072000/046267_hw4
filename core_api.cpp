30c76cb41bc338848b7ab5d09d5f458a2ed0e846
Adam11072000 <ghadbanadam@gmail.com>
2022-06-18 01:09:20 +0300

    passed segel tests

--- core_api.cpp
+++ core_api.cpp
@@ -68,102 +68,23 @@ class Core{
     int total_insts;
     int current_thread;
     bool is_blocked_mt;
+	int thread_num;
 
 public:
     Core(int threads_num, int cs_oh, int lw_cycles, int sw_cycles, bool is_blocked);
     ~Core() = default;
     Core(const Core& src) = default;
+	void run_sim();
+	void next_cycle();
+	void execute_cmd();
+	int find_next_thread_to_run();
 	double get_cpi() const;
     void fill_thread_context(tcontext* context, int tid);
 };
 
 Core::Core(int threads_num, int cs_oh, int lw_cycles, int sw_cycles, bool is_blocked): threads(vector<Thread>(threads_num)),
 cs_overhead(cs_oh), load_cycles(lw_cycles), store_cycles(sw_cycles), total_cycles(0), total_insts(0), current_thread(0),
-is_blocked_mt(is_blocked){
-    bool done = true, is_idle = true;
-    int res, idx, starting_pos;
-    Instruction curr_ins;
-
-    while(true){
-        for(int i = 0; i < threads_num; i++){
-            if(!threads[i].is_finished()) {
-                done = false;
-            }
-        }
-        if(done){
-            break;
-        }
-        // update all threads halt counter, and find ready thread in cyclic iteration.
-        while(is_idle) {
-            for(int i = 0; i < threads_num; i++){
-                if(threads[i].get_halt_counter() != 0){
-                    threads[i].set_halt_counter(threads[i].get_halt_counter() - 1);
-                }
-            }
-            starting_pos = current_thread;
-            for (int i = 0; i < threads_num; i++) { // cyclic iteration to check which thread is ready
-                idx = (i + starting_pos) % threads_num;
-                if (threads[idx].get_halt_counter() == 0 && !threads[idx].is_finished()) {
-                    if (idx != current_thread && is_blocked_mt) { // only for blocked mt
-                        total_cycles += cs_overhead;
-                    }
-                    current_thread = idx;
-                    is_idle = false;
-                    break;
-                }
-            }
-			total_cycles++;
-        }
-        // we have def found a ready thread, starting to check from current_thread.
-        SIM_MemInstRead(threads[current_thread].get_curr_line(), &curr_ins, current_thread);
-        if(curr_ins.opcode == CMD_HALT) {
-            if (!threads[current_thread].is_finished()) {
-                threads[current_thread].set_finished();
-            }
-        } else if(curr_ins.opcode == CMD_NOP){
-
-        } else if(curr_ins.opcode == CMD_ADD){
-            res = threads[current_thread].get_reg(curr_ins.src1_index)
-                    + threads[current_thread].get_reg(curr_ins.src2_index_imm);
-            threads[current_thread].set_reg(curr_ins.dst_index,res);
-        } else if(curr_ins.opcode == CMD_SUB){
-            res = threads[current_thread].get_reg(curr_ins.src1_index)
-                  - threads[current_thread].get_reg(curr_ins.src2_index_imm);
-            threads[current_thread].set_reg(curr_ins.dst_index,res);
-        } else if(curr_ins.opcode == CMD_ADDI){
-            res = threads[current_thread].get_reg(curr_ins.src1_index) + curr_ins.src2_index_imm;
-            threads[current_thread].set_reg(curr_ins.dst_index,res);
-        } else if(curr_ins.opcode == CMD_SUBI){
-            res = threads[current_thread].get_reg(curr_ins.src1_index) - curr_ins.src2_index_imm;
-            threads[current_thread].set_reg(curr_ins.dst_index,res);
-        } else if(curr_ins.opcode == CMD_LOAD){
-            threads[current_thread].set_halt_counter(load_cycles);
-			uint32_t addr = threads[current_thread].get_reg(curr_ins.src1_index);
-			if(curr_ins.isSrc2Imm){
-				addr += curr_ins.src2_index_imm;
-			}else{
-				addr += threads[current_thread].get_reg(curr_ins.src2_index_imm);
-			}
-			int tmp_val = 0;
-			SIM_MemDataRead(addr, &tmp_val);
-			threads[current_thread].set_reg(curr_ins.dst_index, tmp_val);
-        }else{
-			threads[current_thread].set_halt_counter(store_cycles);
-			uint32_t addr = threads[current_thread].get_reg(curr_ins.dst_index);
-			if(curr_ins.isSrc2Imm){
-				addr += curr_ins.src2_index_imm;
-			}else{
-				addr += threads[current_thread].get_reg(curr_ins.src2_index_imm);
-			}
-			SIM_MemDataWrite(addr, threads[current_thread].get_reg(curr_ins.src1_index));
-		}
-        done = true;
-        is_idle = true;
-		threads[current_thread].inc_curr_line();
-		total_cycles++;
-		total_insts++;
-    }
-}
+is_blocked_mt(is_blocked), thread_num(threads_num){}
 
 double Core::get_cpi() const {
 	return (double)total_cycles/ (double) total_insts;
@@ -171,8 +92,116 @@ double Core::get_cpi() const {
 
 void Core::fill_thread_context(tcontext *context, int tid) {
 	for(int i = 0; i < REGS_COUNT; i++){
-		context->reg[i] = threads[tid].get_reg(i);
+		context[tid].reg[i] = threads[tid].get_reg(i);
 	}
+}
+
+void Core::run_sim() {
+	while(true){
+		while(!threads[current_thread].is_finished() && threads[current_thread].get_halt_counter() == 0){
+			execute_cmd();
+			next_cycle();
+			if(threads[current_thread].get_halt_counter() != 0){
+				break;
+			}
+		}
+		current_thread = find_next_thread_to_run();
+		if(current_thread == -1){
+			break;
+		}
+	}
+}
+
+void Core::execute_cmd() {
+	if(threads[current_thread].is_finished()){
+		return;
+	}
+	int res;
+	Instruction curr_ins;
+	SIM_MemInstRead(threads[current_thread].get_curr_line(), &curr_ins, current_thread);
+	if (curr_ins.opcode == CMD_HALT) {
+		if (!threads[current_thread].is_finished()) {
+			threads[current_thread].set_finished();
+		}
+	} else if (curr_ins.opcode == CMD_NOP) {
+
+	} else if (curr_ins.opcode == CMD_ADD) {
+		res = threads[current_thread].get_reg(curr_ins.src1_index)
+			  + threads[current_thread].get_reg(curr_ins.src2_index_imm);
+		threads[current_thread].set_reg(curr_ins.dst_index, res);
+	} else if (curr_ins.opcode == CMD_SUB) {
+		res = threads[current_thread].get_reg(curr_ins.src1_index)
+			  - threads[current_thread].get_reg(curr_ins.src2_index_imm);
+		threads[current_thread].set_reg(curr_ins.dst_index, res);
+	} else if (curr_ins.opcode == CMD_ADDI) {
+		res = threads[current_thread].get_reg(curr_ins.src1_index) + curr_ins.src2_index_imm;
+		threads[current_thread].set_reg(curr_ins.dst_index, res);
+	} else if (curr_ins.opcode == CMD_SUBI) {
+		res = threads[current_thread].get_reg(curr_ins.src1_index) - curr_ins.src2_index_imm;
+		threads[current_thread].set_reg(curr_ins.dst_index, res);
+	} else if (curr_ins.opcode == CMD_LOAD) {
+		threads[current_thread].set_halt_counter(load_cycles + 1);
+		uint32_t addr = threads[current_thread].get_reg(curr_ins.src1_index);
+		if (curr_ins.isSrc2Imm) {
+			addr += curr_ins.src2_index_imm;
+		} else {
+			addr += threads[current_thread].get_reg(curr_ins.src2_index_imm);
+		}
+		int tmp_val = 0;
+		SIM_MemDataRead(addr, &tmp_val);
+		threads[current_thread].set_reg(curr_ins.dst_index, tmp_val);
+	} else {
+		threads[current_thread].set_halt_counter(store_cycles + 1);
+		uint32_t addr = threads[current_thread].get_reg(curr_ins.dst_index);
+		if (curr_ins.isSrc2Imm) {
+			addr += curr_ins.src2_index_imm;
+		} else {
+			addr += threads[current_thread].get_reg(curr_ins.src2_index_imm);
+		}
+		SIM_MemDataWrite(addr, threads[current_thread].get_reg(curr_ins.src1_index));
+	}
+	threads[current_thread].inc_curr_line();
+	total_insts++;
+}
+
+void Core::next_cycle() {
+	total_cycles++;
+	for(int i = 0; i < thread_num; i++){
+		if(!threads[i].is_finished() && threads[i].get_halt_counter() != 0){
+			threads[i].set_halt_counter(threads[i].get_halt_counter() - 1);
+		}
+	}
+}
+
+int Core::find_next_thread_to_run() {
+	bool is_done = true;
+	for(int i = 0; i < thread_num; i++){
+		if(!threads[i].is_finished()){
+			is_done = false;
+		}
+	}
+	if(is_done){
+		return -1;
+	}
+	int idx, to_return = -1;
+	while(to_return == -1) {
+		for (int i = 0; i < thread_num; i++) {
+			idx = (i + current_thread) % thread_num;
+			if (!threads[idx].is_finished() && threads[idx].get_halt_counter() == 0) {
+				to_return = idx;
+				break;
+			}
+		}
+		if (idx != current_thread && is_blocked_mt && to_return != -1) {
+			for (int i = 0; i < cs_overhead; i++) {
+				next_cycle();
+			}
+		}
+		if(to_return == -1) {
+			next_cycle();
+		}
+	}
+	return to_return;
 }
 
 Core* BlockedMTCore = nullptr;
@@ -181,11 +210,13 @@ Core* FineGrainedCore = nullptr;
 void CORE_BlockedMT() {
 	BlockedMTCore = new Core(SIM_GetThreadsNum(), SIM_GetSwitchCycles(),
 							 SIM_GetLoadLat(), SIM_GetStoreLat(), true);
+	BlockedMTCore->run_sim();
 }
 
 void CORE_FinegrainedMT() {
 	FineGrainedCore = new Core(SIM_GetThreadsNum(), SIM_GetSwitchCycles(),
 							   SIM_GetLoadLat(), SIM_GetStoreLat(), false);
+	FineGrainedCore->run_sim();
 }
 
 double CORE_BlockedMT_CPI(){
